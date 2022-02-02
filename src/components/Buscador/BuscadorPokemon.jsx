d7681cc4f47c377fb35fd2e011987ec42b39a0a3
Romina Bocon <rominabocon@hotmail.com>
2022-02-02 16:20:16 -0300

    arreglo lowercase

--- src/components/Buscador/BuscadorPokemon.jsx
+++ src/components/Buscador/BuscadorPokemon.jsx
@@ -10,7 +10,7 @@ export default function BuscadorPokemon() {
             <label htmlFor='buscar_pokemon' className='text-white'>Buscar Pokemon</label>
             <input type="text" className='form-control' id='buscar_pokemon' value={pokemonName}
             onChange={(e) => {
-                setPokemonName(e.target.value)
+                setPokemonName(e.target.value.toLowerCase())
             }}/>
             <button className='btn btn-primary mt-3'
             onClick={() => {
